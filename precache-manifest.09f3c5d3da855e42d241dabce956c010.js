78049d321701c0bbb1845e28354b09a3cd7ea30a
Agnieszka Jarosik <agajarosik@o2.pl>
2019-11-05 18:29:16 +0100

    Updates

---
+++ precache-manifest.09f3c5d3da855e42d241dabce956c010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61ad95c6c6d4612cecafdd32339a1e8",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "084275644a4356244c46",
+    "url": "/Calculator/static/css/main.74301cfc.chunk.css"
+  },
+  {
+    "revision": "424a7f3b430bf8aaac6b",
+    "url": "/Calculator/static/js/2.ee673a96.chunk.js"
+  },
+  {
+    "revision": "084275644a4356244c46",
+    "url": "/Calculator/static/js/main.932d2810.chunk.js"
+  },
+  {
+    "revision": "948835a5ae24ee819954",
+    "url": "/Calculator/static/js/runtime-main.2f606aad.js"
+  }
+]);
